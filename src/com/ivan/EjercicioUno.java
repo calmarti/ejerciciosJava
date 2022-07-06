6c0ae64ac8659866efe3d52e080fa2b4d85fde33
Ivan Martinez <ivmartinezc@gmail.com>
2022-07-06 11:31:03 +0200

    Ejercicio Uno

---
+++ src/com/ivan/EjercicioUno.java
@@ -0,0 +1,28 @@
+package com.ivan;
+
+public class EjercicioUno {
+    public static void main(String[] args) {
+
+        //tipos primitivos
+        short tipo1 = 0;
+        byte tipo2 = 1;
+        int tipo3 = 2;
+        long tipo4 = 3;
+        float tipo5 = 1/3;
+        double tipo6 = 0.33;
+        boolean tipo7 = true;
+        char tipo8 = 'd';
+        String tipo9 = "dato";
+
+        System.out.println("El dato 1 es: " + tipo1);
+        System.out.println("El dato 2 es: " + tipo2);
+        System.out.println("El dato 3 es: " + tipo3);
+        System.out.println("El dato 4 es: " + tipo4);
+        System.out.println("El dato 5 es: " + tipo5);
+        System.out.println("El dato 6 es: " + tipo6);
+        System.out.println("El dato 7 es: " + tipo7);
+        System.out.println("El dato 8 es: " + tipo8);
+        System.out.println("El dato 9 es: " + tipo9);
+
+    }
+}
