984216a3a7603c62197696288254d9a2bb69f508
Ivan Martinez <ivmartinezc@gmail.com>
2022-07-07 18:01:32 +0200

    ejercicio cinco (fix)

--- src/com/ivan/EjercicioCinco/Main.java
+++ src/com/ivan/EjercicioCinco/Main.java
@@ -3,9 +3,11 @@ package com.ivan.EjercicioCinco;
 import oop.herencia.Coche;
 
 public class Main {
-    static CocheCRUD cocheCRUD = new CocheCRUDImpl();
+
     
     public static void main(String[] args) {
+
+        CocheCRUD cocheCRUD = new CocheCRUDImpl();
 
         cocheCRUD.save(new Coche());
         cocheCRUD.findAll();
